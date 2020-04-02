0f7bfb41a9390e12364ec0bf3ea0d308d9355641
golamrabbiazad <golamrabbiazad@gmail.com>
2020-04-03 01:57:37 +0600

    functional pattern, @broken-link,various bug fixes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 // bring values from DOM
-let secondsFromDocument = document.getElementById('seconds');
-let minutesFromDocument = document.getElementById('minutes');
-let hoursFromDocument = document.getElementById('hours');
-let daysFromDocument = document.getElementById('days');
-let btnsFromDocument = document.getElementById('btns');
+let getSecond = document.getElementById('seconds');
+let getMinute = document.getElementById('minutes');
+let getHour = document.getElementById('hours');
+let getDays = document.getElementById('days');
+let hideBtn = document.getElementById('btns');
 
 // define times with details
 const second = 1000;
@@ -15,40 +15,25 @@ const day = hour * 24;
 let getDate = prompt('Enter the Date ex: Aug 20 2020 23:30:20');
 let userDate = new Date(getDate).getTime();
 
-let calculateDate = setInterval(() => {
+let calculateDate = setInterval(intervalImplementation, second);
+
+// the main core-engine of set interval
+function intervalImplementation() {
   let currDate = new Date().getTime();
   let distance = userDate - currDate;
 
-  let calculateDays = Math.floor(distance / day);
-  let calculateHours = Math.floor((distance % day) / hour);
-  let calculateMinutes = Math.floor((distance % hour) / minute);
-  let calculateSeconds = Math.floor((distance % minute) / second);
+  let actualDays = daysFunc(distance, day);
+  let remainHours = hourFunc(distance, day, hour);
+  let minuteLeft = minFunc(distance, hour, minute);
+  let fewSecond = secondFunc(distance, minute, second);
 
-  displayTheCalculation(
-    calculateSeconds,
-    calculateMinutes,
-    calculateHours,
-    calculateDays
-  );
+  updateUI(fewSecond, minuteLeft, remainHours, actualDays);
 
   TimeOver(distance);
-}, second);
+}
 
-let countEnded = () => {
-  let preBtn = `<button class="btn-primary btn-lg">
-        <a href="../event/wish-card/index.html" class="textOnColor">Next</a>
-        </button>`;
-  btnsFromDocument.innerHTML = preBtn;
-};
-
-let setAllCountDownZero = value => {
-  secondsFromDocument.innerHTML = value;
-  minutesFromDocument.innerHTML = value;
-  hoursFromDocument.innerHTML = value;
-  daysFromDocument.innerHTML = value;
-};
-
-let TimeOver = distance => {
+// It is one kind of controller of time become zero
+let TimeOver = (distance, calculateDate) => {
   // when time completed, set all field to 0
   if (distance < 0) {
     clearInterval(calculateDate);
@@ -59,10 +44,47 @@ let TimeOver = distance => {
   }
 };
 
-let displayTheCalculation = (Seconds, Minutes, Hours, Days) => {
-  // show on the display
-  secondsFromDocument.innerHTML = Seconds;
-  minutesFromDocument.innerHTML = Minutes;
-  hoursFromDocument.innerHTML = Hours;
-  daysFromDocument.innerHTML = Days;
+// we are setting all field 0 when time has reached 0
+let setAllCountDownZero = value => {
+  getSecond.innerHTML = value;
+  getMinute.innerHTML = value;
+  getHour.innerHTML = value;
+  getDays.innerHTML = value;
 };
+
+// when time become less than 0 it will automatically generate button
+let countEnded = () => {
+  let nextURL = '../wish-card/index.html';
+  let generatedBtn = `<button class="btn-primary btn-lg">
+        <a href="${nextURL}" class="textOnColor">Next</a>
+        </button>`;
+  hideBtn.innerHTML = generatedBtn;
+};
+
+let updateUI = (Seconds, Minutes, Hours, Days) => {
+  // show on the display
+  getSecond.innerHTML = Seconds;
+  getMinute.innerHTML = Minutes;
+  getHour.innerHTML = Hours;
+  getDays.innerHTML = Days;
+};
+
+// here all the calculated func will be set that are later reuseable
+function daysFunc(distance, day) {
+  return Math.floor(distance / day);
+}
+
+// hours
+function hourFunc(distance, day, hour) {
+  return Math.floor((distance % day) / hour);
+}
+
+// minutes
+function minFunc(distance, hour, minute) {
+  return Math.floor((distance % hour) / minute);
+}
+
+// seconds
+function secondFunc(distance, minute, second) {
+  return Math.floor((distance % minute) / second);
+}
